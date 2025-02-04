bd6e873c9f092a61a30e19d4f658e82846e3cf03
subho004 <haiisubhajit2003@gmail.com>
2025-02-04 15:17:05 +0530

    fix: formatiing

--- app/ui/read-markdown.tsx
+++ app/ui/read-markdown.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
+import remarkGfm from "remark-gfm";
 // import "highlight.js/styles/github.css"; // Syntax highlighting style
 
 interface MarkdownPageProps {
@@ -10,7 +11,10 @@ interface MarkdownPageProps {
 const MarkdownPage: React.FC<MarkdownPageProps> = ({ markdown }) => {
   return (
     <div className="prose mx-auto">
-      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+      <ReactMarkdown
+        rehypePlugins={[rehypeHighlight]}
+        remarkPlugins={[remarkGfm]}
+      >
         {markdown}
       </ReactMarkdown>
     </div>
