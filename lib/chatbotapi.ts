835e50537392807881890d054a80cee4c33a761e
DawoodKhatri <dawoodkhatri18@gmail.com>
2025-03-20 12:31:51 +0530

    Dynamic API Url

--- lib/chatbotapi.ts
+++ lib/chatbotapi.ts
@@ -91,7 +91,7 @@ export async function queryHellerApi(
     try {
       console.log(`Trying API request with config:`, config);
       const response = await fetch(
-        "https://heller-proto-backend-j9yd.onrender.com/chat/query",
+        `${process.env.NEXT_PUBLIC_API_URL}/chat/query`,
         config
       );
 
